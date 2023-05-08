fad4cf411de7feedd6dfed392d6e393ab28fe7be
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-05-08 15:41:34 +0200

    Implement updated navigation design (#5)
    * Implement updated navigation design
    
    * Remove margin top

--- src/features/navigation/items.ts
+++ src/features/navigation/items.ts
@@ -6,6 +6,7 @@ type ConditionFunction = (state: ApplicationState) => boolean;
 export type NavigationItem = {
   label: string;
   tag: string;
+  description: string;
   icon: IconName;
   path: string;
 
@@ -20,18 +21,21 @@ export const NAVIGATION_ITEMS: NavigationItem[] = [
   {
     label: 'JSON-RPC',
     tag: 'onRpcRequest',
+    description: 'Send JSON-RPC requests to the snap',
     icon: 'textBubble',
     path: '/',
   },
   {
     label: 'Cronjobs',
     tag: 'onCronjob',
+    description: 'Schedule and run periodic actions',
     icon: 'alert',
     path: '/cronjobs',
   },
   {
     label: 'Transaction',
     tag: 'onTransaction',
+    description: 'Send transactions to the snap',
     icon: 'textBubble',
     path: '/transactions',
   },
