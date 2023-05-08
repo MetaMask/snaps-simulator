b28581e9562e62d3b052fe39f0be12b1c322a042
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-05-08 13:28:55 +0200

    Set up conditional rendering

--- src/features/navigation/Navigation.tsx
+++ src/features/navigation/Navigation.tsx
@@ -1,6 +1,7 @@
 import { Container, List, Stack, Tag, Text } from '@chakra-ui/react';
 import { FunctionComponent } from 'react';
 
+import { useSelector } from '../../hooks';
 import { Item } from './components';
 import { NAVIGATION_ITEMS } from './items';
 
@@ -9,15 +10,25 @@ import { NAVIGATION_ITEMS } from './items';
  *
  * @returns The navigation component.
  */
-export const Navigation: FunctionComponent = () => (
-  <Container as="nav" size="fullWidth">
-    <Stack as={List} marginTop="4" spacing="4">
-      {NAVIGATION_ITEMS.map(({ icon, label, tag, path }) => (
-        <Item key={path} icon={icon} path={path}>
-          <Text>{label}</Text>
-          <Tag variant="code">{tag}</Tag>
-        </Item>
-      ))}
-    </Stack>
-  </Container>
-);
+export const Navigation: FunctionComponent = () => {
+  const applicationState = useSelector((state) => state);
+
+  return (
+    <Container as="nav" size="fullWidth">
+      <Stack as={List} marginTop="4" spacing="4">
+        {NAVIGATION_ITEMS.map(({ condition, icon, label, tag, path }) => {
+          if (condition && !condition(applicationState)) {
+            return null;
+          }
+
+          return (
+            <Item key={path} icon={icon} path={path}>
+              <Text>{label}</Text>
+              <Tag variant="code">{tag}</Tag>
+            </Item>
+          );
+        })}
+      </Stack>
+    </Container>
+  );
+};
