894c2faef8a2bedd6b4df2399b672e81703034e2
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-05-10 17:17:30 +0200

    Hide play button on history tab

--- src/features/handlers/components/Handler.tsx
+++ src/features/handlers/components/Handler.tsx
@@ -8,7 +8,7 @@ import {
   TabPanels,
   Tabs,
 } from '@chakra-ui/react';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Icon } from '../../../components';
@@ -16,74 +16,81 @@ import { History } from './History';
 import { Response } from './Response';
 import { UserInterface } from './UserInterface';
 
-export const Handler: FunctionComponent = () => (
-  <Flex width="100%" direction="column" overflow="hidden">
-    <Flex direction="row" flex="1" overflow="hidden">
-      <Flex direction="column" flex="1" width="50%" overflow="hidden">
-        <Tabs
-          display="flex"
-          flexDirection="column"
-          flex="1"
-          overflow="hidden"
-          isLazy={true}
-        >
-          <TabList alignItems="center">
-            <Tab>Request</Tab>
-            <Tab>History</Tab>
-            <Box marginLeft="auto">
-              <Button
-                variant="unstyled"
-                type="submit"
-                minWidth="0"
-                form="request-form"
-              >
-                <Icon icon="play" width="24px" />
-              </Button>
-            </Box>
-          </TabList>
-          <TabPanels
+export const Handler: FunctionComponent = () => {
+  const [tab, setTab] = useState(0);
+
+  return (
+    <Flex width="100%" direction="column" overflow="hidden">
+      <Flex direction="row" flex="1" overflow="hidden">
+        <Flex direction="column" flex="1" width="50%" overflow="hidden">
+          <Tabs
             display="flex"
             flexDirection="column"
             flex="1"
             overflow="hidden"
+            isLazy={true}
+            onChange={setTab}
           >
-            <TabPanel display="flex" flexDirection="column" flex="1">
-              <Outlet />
-            </TabPanel>
-            <TabPanel padding="0" overflowY="auto">
-              <History />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Flex>
-      <Box
-        display="flex"
-        flexDirection="column"
-        flex="1"
-        width="50%"
-        borderLeft="1px solid"
-        borderColor="border.default"
-      >
-        <Tabs display="flex" flexDirection="column" flex="1">
-          <TabList>
-            <Tab>Response</Tab>
-            <Tab>UI</Tab>
-          </TabList>
-          <TabPanels display="flex" flexDirection="column" flex="1">
-            <TabPanel
+            <TabList alignItems="center">
+              <Tab>Request</Tab>
+              <Tab>History</Tab>
+              {tab === 0 && (
+                <Box marginLeft="auto">
+                  <Button
+                    variant="unstyled"
+                    type="submit"
+                    minWidth="0"
+                    form="request-form"
+                  >
+                    <Icon icon="play" width="24px" />
+                  </Button>
+                </Box>
+              )}
+            </TabList>
+            <TabPanels
               display="flex"
               flexDirection="column"
               flex="1"
-              padding="0"
+              overflow="hidden"
             >
-              <Response />
-            </TabPanel>
-            <TabPanel>
-              <UserInterface />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Box>
+              <TabPanel display="flex" flexDirection="column" flex="1">
+                <Outlet />
+              </TabPanel>
+              <TabPanel padding="0" overflowY="auto">
+                <History />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Flex>
+        <Box
+          display="flex"
+          flexDirection="column"
+          flex="1"
+          width="50%"
+          borderLeft="1px solid"
+          borderColor="border.default"
+        >
+          <Tabs display="flex" flexDirection="column" flex="1">
+            <TabList>
+              <Tab>Response</Tab>
+              <Tab>UI</Tab>
+            </TabList>
+            <TabPanels display="flex" flexDirection="column" flex="1">
+              <TabPanel
+                display="flex"
+                flexDirection="column"
+                flex="1"
+                padding="0"
+              >
+                <Response />
+              </TabPanel>
+              <TabPanel>
+                <UserInterface />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </Flex>
     </Flex>
-  </Flex>
-);
+  );
+};
