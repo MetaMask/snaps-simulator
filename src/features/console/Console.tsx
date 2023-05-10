55e85a85bd21d7dc0aed58d0c962da2d7129d7e2
Frederik Bolding <frederik.bolding@gmail.com>
2023-05-10 17:57:17 +0200

    Scroll to the bottom when receiving a new console log

--- src/features/console/Console.tsx
+++ src/features/console/Console.tsx
@@ -6,42 +6,58 @@ import {
   TabPanels,
   Tabs,
 } from '@chakra-ui/react';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect, useRef } from 'react';
 
+import { useSelector } from '../../hooks';
 import { ConsoleContent } from './ConsoleContent';
+import { getConsoleEntries } from './slice';
 
 /**
  * Console component.
  *
  * @returns The console component.
  */
-export const Console: FunctionComponent = () => (
-  <Flex
-    borderTop="1px solid"
-    borderColor="border.default"
-    height="266px"
-    flexDirection="column"
-    overflow="hidden"
-  >
-    <Tabs display="flex" flexDirection="column" flex="1" overflow="hidden">
-      <TabList>
-        <Tab>Console</Tab>
-      </TabList>
-      <TabPanels
-        display="flex"
-        flexDirection="column"
-        flex="1"
-        overflow="hidden"
-      >
-        <TabPanel
+export const Console: FunctionComponent = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const entries = useSelector(getConsoleEntries);
+
+  useEffect(() => {
+    if (ref.current) {
+      // TODO: Maybe not scroll if the user is scrolled up?
+      ref.current.scrollTop = ref.current.scrollHeight;
+    }
+  }, [entries]);
+
+  return (
+    <Flex
+      borderTop="1px solid"
+      borderColor="border.default"
+      height="266px"
+      flexDirection="column"
+      overflow="hidden"
+    >
+      <Tabs display="flex" flexDirection="column" flex="1" overflow="hidden">
+        <TabList>
+          <Tab>Console</Tab>
+        </TabList>
+        <TabPanels
           display="flex"
           flexDirection="column"
           flex="1"
-          overflowY="auto"
+          overflow="hidden"
         >
-          <ConsoleContent />
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
-  </Flex>
-);
+          <TabPanel
+            ref={ref}
+            display="flex"
+            flexDirection="column"
+            flex="1"
+            overflowY="auto"
+          >
+            <ConsoleContent />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Flex>
+  );
+};
