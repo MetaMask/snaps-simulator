45e9b92a3b26d232f3e78952e5336dce7315897e
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-05-11 16:47:33 +0200

    Add manifest empty state

--- src/features/manifest/components/Validation.tsx
+++ src/features/manifest/components/Validation.tsx
@@ -1,12 +1,43 @@
-import { List } from '@chakra-ui/react';
+import { Center, Heading, List, Text } from '@chakra-ui/react';
 import { FunctionComponent } from 'react';
 
+import { Icon } from '../../../components';
 import { useSelector } from '../../../hooks';
+import { getSnapManifest } from '../../simulation';
 import { getManifestResults } from '../slice';
 import { Item } from './Item';
 
 export const Validation: FunctionComponent = () => {
+  const manifest = useSelector(getSnapManifest);
   const results = useSelector(getManifestResults);
+
+  if (!manifest) {
+    return (
+      <Center
+        background="background.alternative"
+        flex="1"
+        flexDirection="column"
+      >
+        <Icon icon="moon" width="34px" height="auto" marginBottom="1.5" />
+        <Heading
+          as="h3"
+          fontSize="sm"
+          fontWeight="700"
+          color="text.muted"
+          marginBottom="1"
+        >
+          Manifest can&apos;t be found
+        </Heading>
+        <Text fontSize="xs" textAlign="center" color="text.muted">
+          Make sure you’re connected to the snap
+          <br />
+          and have a <i>snap.manifest.json</i>
+          <br />
+          file in the root of you repository.
+        </Text>
+      </Center>
+    );
+  }
 
   return (
     <List>
