0713e24786b33fbe050dc9c1446c15a3cd205174
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-05-11 14:41:56 +0200

    Fix snap indicator

--- src/features/status/StatusIndicator.tsx
+++ src/features/status/StatusIndicator.tsx
@@ -1,4 +1,5 @@
 import { Flex, Spinner, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
 import { Icon } from '../../components';
 import { useDispatch, useSelector } from '../../hooks';
@@ -9,6 +10,19 @@ export const StatusIndicator = () => {
   const snapUrl = useSelector(getSnapUrl);
   const status = useSelector(getStatus);
   const dispatch = useDispatch();
+
+  const [prettyUrl, setPrettyUrl] = useState(snapUrl);
+
+  useEffect(() => {
+    if (snapUrl) {
+      try {
+        const url = new URL(snapUrl);
+        setPrettyUrl(url.host);
+      } catch {
+        // Ignore.
+      }
+    }
+  }, [snapUrl]);
 
   const color =
     // eslint-disable-next-line no-nested-ternary
@@ -23,14 +37,29 @@ export const StatusIndicator = () => {
   };
 
   return (
-    <Flex alignItems="center" gap="2" onClick={handleClick} cursor="pointer">
+    <Flex
+      alignItems="center"
+      gap="2"
+      onClick={handleClick}
+      cursor="pointer"
+      _hover={{
+        p: {
+          color,
+        },
+      }}
+    >
       {status === SnapStatus.Ok && <Icon icon="dot" width="8px" />}
       {status === SnapStatus.Loading && <Spinner color={color} size="xs" />}
       {status === SnapStatus.Error && (
         <Icon icon="errorTriangle" width="16px" />
       )}
-      <Text fontWeight="600" textColor={color}>
-        {snapUrl}
+      <Text
+        fontWeight="500"
+        fontSize="sm"
+        color="text.muted"
+        transition="color ease 0.2s"
+      >
+        {prettyUrl}
       </Text>
     </Flex>
   );
